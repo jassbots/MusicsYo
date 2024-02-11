3679e71be54f7f03a5a6ae41d540c1a8d4df11be
ɴᴏᴏʙ/Sidhu <132650820+Sidhusaab0001@users.noreply.github.com>
2024-02-12 00:28:06 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://telegra.ph/file/6e8799dbe2145b1ca7e83.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await DAXX.ping()
