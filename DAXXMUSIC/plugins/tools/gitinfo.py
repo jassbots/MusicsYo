8b1732fbe619890d0ce093a963df3a01fa1adca8
ɴᴏᴏʙ sɪᴅʜᴜ <132650820+Sidhusaab0001@users.noreply.github.com>
2024-02-16 22:03:46 +0530

    Update gitinfo.py

--- DAXXMUSIC/plugins/tools/gitinfo.py
+++ DAXXMUSIC/plugins/tools/gitinfo.py
@@ -6,12 +6,12 @@ from DAXXMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 ###
-@app.on_message(filters.command("pink"))
+@app.on_message(filters.command("blackpink"))
 async def daxxhub(_, message):
-    text = message.text[len("/pink") :]
-    papadaxx(f"{text}").save(f"pink_{message.from_user.id}.png")
-    await message.reply_photo(f"pink_{message.from_user.id}.png")
-    os.remove(f"pink_{message.from_user.id}.png")
+    text = message.text[len("/blackpink") :]
+    papadaxx(f"{text}").save(f"blackpink_{message.from_user.id}.png")
+    await message.reply_photo(f"blackpink_{message.from_user.id}.png")
+    os.remove(f"blackpink_{message.from_user.id}.png")
 ####
 
 @app.on_message(filters.command(["github", "git"]))
