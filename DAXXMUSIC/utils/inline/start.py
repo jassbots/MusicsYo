e338adbf4df8e90d5bf77730613c4fc411d0fbd8
ɴᴏᴏʙ/Sidhu <132650820+Sidhusaab0001@users.noreply.github.com>
2024-02-16 14:02:42 +0530

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -31,5 +31,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="shizu_source"),
+        ],
     ]
     return buttons
